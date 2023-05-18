2bfbb53655332b356399dab4f922afd00134cc24
Ethan Coward <ethan@ethancoward.dev>
2023-05-18 11:20:14 +0100

    Update styling

--- src/app/hidden/page.tsx
+++ src/app/hidden/page.tsx
@@ -1,9 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 
 export default function HiddenPage() {
+    const { userId, user } = auth();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold">Hello World!</h1>
+            <p className="text-2xl font-bold">Your user ID is {userId}</p>
             <p className="text-2xl font-bold">This is a hidden page!</p>
             <UserButton />
         </main>
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-    return <SignIn />;
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <SignIn />
+        </main>
+    );
 }
