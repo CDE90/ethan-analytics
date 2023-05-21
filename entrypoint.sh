7d013314030825e3df9badbb0a34bba95930120a
Ethan Coward <ethan@ethancoward.dev>
2023-05-21 15:21:34 +0100

    Add stuffs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-cargo --version
+# cargo --version
 
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
--- src/main.rs
+++ src/main.rs
@@ -87,7 +87,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(event_handler)
     })
-    .bind(("127.0.0.1", 3001))?
+    .bind(("0.0.0.0", 3001))?
     .run()
     .await
 }
