6454e536e338703f0d995bdfaffd440698187ac7
CDE90 <ethan@ethancoward.dev>
2023-06-11 21:48:39 +0100

    Add basic charts

--- src/app/d/[host]/page.tsx
+++ src/app/d/[host]/page.tsx
@@ -1,6 +1,157 @@
-import { eq } from "drizzle-orm";
+import { eq, gte, and, lte } from "drizzle-orm";
 import { db } from "~/server/db";
 import { websites, events } from "~/server/schema";
+import { AreaChart, Card, Title } from "@tremor/react";
+
+type GroupBy = "hour" | "day" | "week" | "month" | "year";
+
+async function getData(host: string, groupBy: GroupBy) {
+    let dateFrom;
+    switch (groupBy) {
+        case "hour":
+            dateFrom = new Date(Date.now() - 24 * 60 * 60 * 1000);
+            break;
+        case "day":
+            dateFrom = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+            break;
+        case "week":
+            dateFrom = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+            break;
+        case "month":
+            dateFrom = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000);
+            break;
+        case "year":
+            dateFrom = new Date(Date.now() - 5 * 365 * 24 * 60 * 60 * 1000);
+            break;
+    }
+
+    const eventsData = await db
+        .select({
+            "id": events.id,
+            "eventType": events.eventType,
+            "host": events.host,
+            "page": events.page,
+            "referrer": events.referrer,
+            "timestamp": events.timestamp,
+        })
+        .from(events)
+        .where(
+            and(
+                gte(events.timestamp, dateFrom),
+                lte(events.timestamp, new Date(Date.now())),
+                eq(events.host, host)
+            )
+        )
+        .orderBy(events.timestamp)
+        .execute();
+
+    return eventsData;
+}
+
+interface Event {
+    timestamp: Date | null;
+}
+
+function getMonday(d: Date) {
+    const day = d.getDay()
+    const diff = d.getDate() - day + (day === 0 ? -6 : 1); // adjust when day is sunday
+
+    return new Date(d.setDate(diff));
+}
+
+function getMonth(d: Date) {
+    return new Date(d.getFullYear(), d.getMonth(), 1);
+}
+
+function groupData<T extends Event>(data: T[], groupBy: GroupBy): { [key: string]: T[] } {
+    switch (groupBy) {
+        case "hour":
+            return data.reduce((acc, event) => {
+                const hour = `${event.timestamp?.getHours()}:00`;
+
+                if (hour) {
+                    if (acc[hour]) {
+                        acc[hour]?.push(event);
+                    } else {
+                        acc[hour] = [event];
+                    }
+                }
+                return acc;
+            }
+            , {} as { [key: string]: typeof data });
+        case "day":
+            return data.reduce((acc, event) => {
+                const day = event.timestamp?.toLocaleDateString();
+
+                if (day) {
+                    if (acc[day]) {
+                        acc[day]?.push(event);
+                    } else {
+                        acc[day] = [event];
+                    }
+                }
+                return acc;
+            }
+            , {} as { [key: string]: typeof data });
+        case "week":
+            return data.reduce((acc, event) => {
+                // get the monday of the week
+                const week = getMonday(event.timestamp!).toLocaleDateString();
+
+                if (week) {
+                    if (acc[week]) {
+                        acc[week]?.push(event);
+                    } else {
+                        acc[week] = [event];
+                    }
+                }
+                return acc;
+            }
+            , {} as { [key: string]: typeof data });
+        case "month":
+            return data.reduce((acc, event) => {
+                const month = getMonth(event.timestamp!).toLocaleDateString();
+
+                if (month) {
+                    if (acc[month]) {
+                        acc[month]?.push(event);
+                    } else {
+                        acc[month] = [event];
+                    }
+                }
+                return acc;
+            }
+            , {} as { [key: string]: typeof data });
+        case "year":
+            return data.reduce((acc, event) => {
+                const year = event.timestamp?.getFullYear().toString();
+
+                if (year) {
+                    if (acc[year]) {
+                        acc[year]?.push(event);
+                    } else {
+                        acc[year] = [event];
+                    }
+                }
+                return acc;
+            }
+            , {} as { [key: string]: typeof data });
+    }
+}
+
+
+
+function getChartData(data: { [key: number]: Event[] }, groupBy: GroupBy) {
+    const chartData = Object.entries(data).map(([date, data]) => {
+        return {
+            date: date,
+            events: data.length,
+        };
+    });
+
+    return chartData;
+}
+
 
 export default async function DashboardPage({
     params,
@@ -25,24 +176,26 @@ export default async function DashboardPage({
         return <div>Website not found</div>;
     }
 
-    const eventsData = await db
-        .select()
-        .from(events)
-        .where(eq(events.host, host))
-        .execute();
+    const groupBy: GroupBy = "day";
 
-    // count how many events there are per day
-    const eventsPerDay: { [key: string]: number } = {};
-    eventsData.forEach((event) => {
-        const date = event.timestamp?.toLocaleDateString();
-        if (date) {
-            if (eventsPerDay[date]) {
-                eventsPerDay[date] += 1;
-            } else {
-                eventsPerDay[date] = 1;
-            }
-        }
-    });
+    const eventsData = await getData(host, groupBy);
+
+    const eventsByHour = groupData(eventsData, groupBy);
+
+    const chartData = getChartData(eventsByHour, groupBy);
+
+    // TODO: ensure that the full date range is covered, even if there are no events
+    // TODO: add a date picker to select the date range (store as a query param)
+    // TODO: add a dropdown to select the group by (hour, day, week, month, year) (store as a query param)
+    // maybe switch to some sane defaults such as:
+    // - last 24 hours, grouped by hour
+    // - last 7 days, grouped by day
+    // - last 30 days, grouped by day
+    // - month to date, grouped by day
+    // - year to date, grouped by month
+    // - last 12 months, grouped by month
+    // - all time, grouped by month (quarter, year? custom?) (only from the time the website was added to the dashboard)
+    // - custom date range, automatically grouped by how many days are in the range (or maybe a dropdown to select the group by)
 
     return (
         <div>
@@ -50,6 +203,23 @@ export default async function DashboardPage({
             <p>Host: {params.host}</p>
             <p>Website ID: {websiteData[0]?.id}</p>
             <p>Website Name: {websiteData[0]?.name}</p>
+            <Card>
+                <Title>Events by {
+                    groupBy === "hour" ? "Hour" :
+                        groupBy === "day" ? "Day" :
+                            groupBy === "week" ? "Week" :
+                                groupBy === "month" ? "Month" :
+                                    groupBy === "year" ? "Year" : ""
+
+                    }</Title>
+                <AreaChart
+                    className="h-72 mt-4"
+                    data={chartData}
+                    index="date"
+                    categories={["events"]}
+                    colors={["indigo"]}
+                />
+            </Card>
             <div className="flex flex-wrap">
                 {eventsData.map((event) => (
                     <div
